905d09cd4219c1b173fe5f35dffc15415e319bd8
A71as <114636098+A71as@users.noreply.github.com>
2023-11-13 22:49:42 -0500

    Delete MGS2.0.py

--- MGS2.0.py
+++
@@ -1,9 +0,0 @@
-print("***** Welcome to the Music Grading System 1.0 *****")
-print("***** Created by Ahmed Alami, GitHub: A71as *****")
-
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-
-spotipy.VERSION
